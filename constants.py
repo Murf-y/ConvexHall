19777c0f0d9a4e19b34b08eb335418343aa4e697
Murf-y <charbelfayad64@gmail.com>
2021-09-17 21:42:48 +0300

    fixed button bug

---
+++ constants.py
@@ -0,0 +1,21 @@
+
+# COLORS
+white = (230, 230, 230)
+black = (0, 0, 0)
+grey = (13, 17, 23)
+red = (255, 0, 0)
+green = (34, 204, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+purple = (166, 77, 255)
+orange = (255, 162, 0)
+
+# variables
+WIDTH = 1200
+HEIGHT = 650
+BUFFER = 50
+LOWERGAP = 50
+normal_point_width = 10
+outer_point_width = 20
+main_lines_width = 10
+number_of_points = 500
